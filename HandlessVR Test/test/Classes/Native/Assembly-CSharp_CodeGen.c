e6c4ab266a8ea5b5366ab9671dd7ecdb72814782
Jack <jackyangzzh@gmail.com>
2020-11-25 19:57:20 -0600

    Finally get it to work with cardboard

---
+++ HandlessVR Test/test/Classes/Native/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,91 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void CameraPointer::Update()
+extern void CameraPointer_Update_m8A821F12B5D9EABAF896D8F44D90BA35CDF18A80 (void);
+// 0x00000002 System.Void CameraPointer::.ctor()
+extern void CameraPointer__ctor_m1807B6267747B5E7D59E5D15639C7F74682A1ABD (void);
+// 0x00000003 System.Void CardboardStartup::Start()
+extern void CardboardStartup_Start_m751D99C31E9C6DC40E2167E1F831BC50C6792FA9 (void);
+// 0x00000004 System.Void CardboardStartup::Update()
+extern void CardboardStartup_Update_mBB2ED1D333C336ABEE4FBB35572C2D1D825227C4 (void);
+// 0x00000005 System.Void CardboardStartup::.ctor()
+extern void CardboardStartup__ctor_m3B505DFA127E48CCB1905CAD3CBE31B0CD035489 (void);
+// 0x00000006 System.Void ObjectController::Start()
+extern void ObjectController_Start_m6CBC7831D787046497EF285A7F9F48226FABA447 (void);
+// 0x00000007 System.Void ObjectController::TeleportRandomly()
+extern void ObjectController_TeleportRandomly_mB70295EE9A69E76BB6CC366D9DE131AC33F4E4F2 (void);
+// 0x00000008 System.Void ObjectController::OnPointerEnter()
+extern void ObjectController_OnPointerEnter_m8F53653AB123FA2EBFAA6907EF7A87059321959E (void);
+// 0x00000009 System.Void ObjectController::OnPointerExit()
+extern void ObjectController_OnPointerExit_m07CD164A708423FA8CB2CCF963518D1A5A539510 (void);
+// 0x0000000A System.Void ObjectController::OnPointerClick()
+extern void ObjectController_OnPointerClick_mB77A0CFC61186F30F687B8512A5169D56679491B (void);
+// 0x0000000B System.Void ObjectController::SetMaterial(System.Boolean)
+extern void ObjectController_SetMaterial_m83603C07E2B64F9FD82B29CD5C4932FDB660DBF0 (void);
+// 0x0000000C System.Void ObjectController::.ctor()
+extern void ObjectController__ctor_m7D9F744FBC984F5926A20FA4FE9FCE1DE5E658C4 (void);
+static Il2CppMethodPointer s_methodPointers[12] = 
+{
+	CameraPointer_Update_m8A821F12B5D9EABAF896D8F44D90BA35CDF18A80,
+	CameraPointer__ctor_m1807B6267747B5E7D59E5D15639C7F74682A1ABD,
+	CardboardStartup_Start_m751D99C31E9C6DC40E2167E1F831BC50C6792FA9,
+	CardboardStartup_Update_mBB2ED1D333C336ABEE4FBB35572C2D1D825227C4,
+	CardboardStartup__ctor_m3B505DFA127E48CCB1905CAD3CBE31B0CD035489,
+	ObjectController_Start_m6CBC7831D787046497EF285A7F9F48226FABA447,
+	ObjectController_TeleportRandomly_mB70295EE9A69E76BB6CC366D9DE131AC33F4E4F2,
+	ObjectController_OnPointerEnter_m8F53653AB123FA2EBFAA6907EF7A87059321959E,
+	ObjectController_OnPointerExit_m07CD164A708423FA8CB2CCF963518D1A5A539510,
+	ObjectController_OnPointerClick_mB77A0CFC61186F30F687B8512A5169D56679491B,
+	ObjectController_SetMaterial_m83603C07E2B64F9FD82B29CD5C4932FDB660DBF0,
+	ObjectController__ctor_m7D9F744FBC984F5926A20FA4FE9FCE1DE5E658C4,
+};
+static const int32_t s_InvokerIndices[12] = 
+{
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	31,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	12,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
