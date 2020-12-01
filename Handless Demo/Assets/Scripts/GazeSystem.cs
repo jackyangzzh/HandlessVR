5572344d0006759c921a1153b253e73a30569e94
Jack <jackyangzzh@gmail.com>
2020-11-30 20:02:27 -0600

    Add basic reticle

---
+++ Handless Demo/Assets/Scripts/GazeSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessGaze();
+    }
+
+    public void ProcessGaze(){
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * 100);
+
+        if(Physics.Raycast(ray, out hit)){
+
+        }
+    }
+}
