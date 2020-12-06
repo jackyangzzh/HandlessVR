630ddaa38fa389f1e00cf8bacb608ed1fcd2b513
Jack <jackyangzzh@gmail.com>
2020-12-06 09:37:10 -0600

    GazeSystem in progress

---
+++ Handless Demo/Assets/Scripts/GazableObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazableObject : MonoBehaviour
+{
+    public virtual void OnGazeEnter(RaycastHit hitInfo){
+
+    }
+
+    public virtual void OnGaze(RaycastHit hitInfo){
+
+    }
+
+    public virtual void OnGazeExit(){
+
+    }
+
+    public virtual void OnPress(RaycastHit hitInfo){
+
+    }
+
+    public virtual void OnHold(RaycastHit hitInfo){
+
+    }
+
+    public virtual void OnRelease(){
+        
+    }
+}
--- Handless Demo/Assets/Scripts/GazeSystem.cs
+++ Handless Demo/Assets/Scripts/GazeSystem.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class GazeSystem : MonoBehaviour
 {
+    [SerializeField] GameObject reticle;
+
+    private Color inactiveColor = Color.gray;
+    private Color activeColor = Color.green;
+    private GazableObject currentGazeObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetReticleColor(inactiveColor);
     }
 
     // Update is called once per frame
@@ -18,20 +24,47 @@ public class GazeSystem : MonoBehaviour
 
     public void ProcessGaze(){
         Ray ray = new Ray(transform.position, transform.forward);
-        RaycastHit hit;
+        RaycastHit hitInfo;
 
         Debug.DrawRay(ray.origin, ray.direction * 100);
 
-        if(Physics.Raycast(ray, out hit)){
+        if(Physics.Raycast(ray, out hitInfo)){
 
+            GameObject hitObj = hitInfo.collider.gameObject;
+            GazableObject gazeObj = hitObj.GetComponent<GazableObject>();
+
+            if(gazeObj != null){
+                if(gazeObj != currentGazeObject){
+                    ClearCurrentObject();
+                    currentGazeObject = gazeObj;
+                    currentGazeObject.OnGazeEnter(hitInfo);
+                    SetReticleColor(activeColor);
+                }else{
+                    ClearCurrentObject();
+                }
+            }
+            else{
+                currentGazeObject.OnGaze(hitInfo);
+            }
+        }else{
+            ClearCurrentObject();
         }
     }
 
     private void SetReticleColor(Color reticleColor){
-
+        reticle.GetComponent<Renderer>().material.SetColor("_Color", reticleColor);
     }
 
     private void CheckForInput(RaycastHit hitInfo){
         
     }
+
+    private void ClearCurrentObject(){
+        if(currentGazeObject != null){
+            currentGazeObject.OnGazeExit();
+            SetReticleColor(inactiveColor);
+            currentGazeObject = null;
+        }
+    }
+    
 }
