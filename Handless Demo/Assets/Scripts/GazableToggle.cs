836c77219ab9b0706da92859d10d7b76ba524128
Jack <jackyangzzh@gmail.com>
2020-12-14 20:49:17 -0600

    Some small updates

---
+++ Handless Demo/Assets/Scripts/GazableToggle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GazableToggle : GazableObject
+{
+    private Toggle toggle;
+    private void Awake()
+    {
+        toggle = GetComponent<Toggle>();
+    }
+
+    private void Start()
+    {
+        toggle.isOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnPress(RaycastHit hitInfo)
+    {
+        if (toggle.isOn)
+        {
+            toggle.isOn = false;
+        }
+        else
+        {
+            toggle.isOn = true;
+        }
+
+    }
+}
